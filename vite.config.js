b44065e4a6f912d1b19b8c7475e74bf62cc1cc3b
mhamad1235 <mhamadsalliim@gmail.com>
2025-10-28 16:56:30 +0300

    path issue

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
-    allowedHosts: true // Allow all hosts (for development only) OR
-    // Alternatively, you can specify an array of allowed hosts:
-    // allowedHosts: ['5c715fd6886c.ngrok-free.app', 'localhost']
-  }
+  base: '/',
 })
